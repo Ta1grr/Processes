5c40568a66addc779de5cf58da7e3752aa4e6785
Ta1grr <taile602@gmail.com>
2019-04-04 18:15:46 -0500

    I got it outputting the cpu process

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -14,6 +14,7 @@ and `clock_gettime()` should work just fine.
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/wait.h>
 
 #define number_iter 1000000
 #define BILLION 1000000000L
@@ -21,6 +22,57 @@ and `clock_gettime()` should work just fine.
 int main()
 {
     // Your code here
+    uint64_t diff;
+	struct timespec start, end;
+	int i;
+
+    // char inbuf[number_iter];
+    // int p[2];
+
+    // int pid = fork();
+
+    // if (pid < 0) {
+    //     perror("fork");
+    //     exit(1);
+    // }
+    // else if (pid == 0) {
+    //     /* measure monotonic time */
+    //     clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+    //     for (i = 0; i < number_iter; i++) {
+    //     write(p[1], &inbuf, number_iter);
+    //     }	/* do stuff */
+    //     clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
+    // }
+    // else {
+    //     diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+    //     printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
+    //     // wait(NULL);
+    // }
+
+    int sums = 0;
+
+	// /* now re-do this and measure CPU time */
+	// /* the time spent sleeping will not count (but there is a bit of overhead */
+	// clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);	/* mark start time */
+	// sleep(1);	/* do stuff */
+	// clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);		/* mark the end time */
+
+	// diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+	// printf("elapsed process CPU time = %llu nanoseconds\n", (long long unsigned int) diff);
+
+    for (int i = 0; i < number_iter; i++) {
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);	/* mark start time */
+        sleep(1);	/* do stuff */
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);		/* mark the end time */
+
+        diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+        sums = sums + diff;
+    }
+    int average = sums / number_iter;
+    printf("Average time: %d", average);
+
+    // close(p[0]); 
+    // close(p[1]); 
     
     return 0;
 }
